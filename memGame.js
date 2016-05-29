52e9d100cb4fa5573528962201e70353d3017918
coderick14 <bhattacharyya.rick14@gmail.com>
2016-05-30 02:11:05 +0530

    Added countdown timer,debugged reveal()

--- memGame.js
+++ memGame.js
@@ -1,5 +1,7 @@
-var arr=[],solved=[],activeIndex=null,activeObj=null,countSolved=0,score=0,noOfMoves=0;
-var tt,mins=0,secs=0,gameStarted=0;//gameStarted waits for the first click to start the timer
+var arr=[],solved=[],rotated=[];
+var curIndex,curObj,activeIndex=null,activeObj=null;
+var countSolved=0,score=0,noOfMoves=0;
+var tt,secs=60,gameStarted=0;//gameStarted waits for the first click to start the timer
 //tt sets and clears the interval for timer
 for(i=0;i<15;i=i+2)
 {
@@ -7,7 +9,7 @@ for(i=0;i<15;i=i+2)
 }
 for(i=0;i<16;i++)
 {
-  solved[i]=false;
+  solved[i]=rotated[i]=false;
 }
 
  //Fisher-Yates algorithm to randomly shuffle the array
@@ -26,35 +28,39 @@ function shuffle()
 //function to display the time taken to finish the game
 function timer()
 {
-  secs++;
-  if(secs>=60)
+  secs--;
+  document.getElementById('time').textContent='00:'+((secs<10)?'0'+secs:secs);
+  if(secs==0)
   {
-    secs=0;
-    mins++;
+    clearInterval(tt);
+    window.alert('Time up!! Click OK to see your score.');
+    document.getElementById('score').textContent=score;
+    document.getElementById('moves').textContent=noOfMoves;
+
   }
-  document.getElementById('time').textContent=((mins<10)?'0'+mins:mins)+':'+((secs<10)?'0'+secs:secs);
 }
 
 //function to check whether the two consecutive boxes have the same value
 function check(i,obj)
 {
-  if(arr[activeIndex]==arr[i])
+  if(arr[curIndex]==arr[i])
   {
-    solved[activeIndex]=solved[i]=true;
+    solved[curIndex]=solved[i]=true;
     countSolved+=2;
-    document.getElementById('score').textContent= ++score;
+    ++score;
   }
   else {
     hide(obj,i);
-    hide(activeObj,activeIndex);
+    hide(curObj,curIndex);
   }
-  activeIndex=activeObj=null;
-  document.getElementById('moves').textContent= ++noOfMoves;
-  if(countSolved==16)
+
+  ++noOfMoves;
+  if(countSolved==16 && secs!=0)
   {
     clearInterval(tt);
-    window.alert("Congratulations!!You solved it with "+noOfMoves+" moves in a time of "+document.getElementById('time').textContent);
-    location.reload();
+    window.alert("Congratulations!!You solved it in "+(60-secs)+" seconds. Click OK to see your score");
+    document.getElementById('score').textContent=score;
+    document.getElementById('moves').textContent=noOfMoves;
   }
 }
 
@@ -68,7 +74,6 @@ function reveal(obj,i)
   }
   if(!solved[i] && i!=activeIndex) {
     console.log(i);
-    obj.style.transform='rotateY(360deg)';
     obj.style.backgroundColor='lightgray';
     obj.style.color='black';
     obj.textContent=arr[i];
@@ -77,17 +82,25 @@ function reveal(obj,i)
         activeIndex=i;
         activeObj=obj;
     }
-    else
+    else {
+      curIndex=activeIndex;
+      curObj=activeObj;
+      activeIndex=activeObj=null;
       setTimeout(check,1500,i,obj);
+    }
   }
 }
 
 //function to hide the boxes which don't match
 function hide(obj,i) {
-    obj.style.transform='rotateY(0deg)';
+    if(!rotated[i])
+      obj.style.transform='rotateY(360deg)';
+    else
+      obj.style.transform='rotateY(0deg)';
     obj.style.backgroundColor='darkslategray';
     obj.style.color='white';
     obj.textContent='?';
+    rotated[i]=!rotated[i];
 }
 
 shuffle();
